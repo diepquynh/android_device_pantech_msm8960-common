5704ade3619e1e768ad22418d1b496dafafb3b2b
Remilia Scarlet <remilia.1505@gmail.com>
2018-02-27 04:31:49 +0000

    msm8960-common: Reorganize common device tree
    * Remove NFC configs for now because we don't have working NFC on LP
    * Use makefiles to treat config objects
    * Reorganize BoardConfig and msm8960 flags
    * Import common flags from ef52l
    * Import RIL, etc scripts, audio, wifi configs from ef52l
    
    Change-Id: Ied725d63e970f2521271fe14b58796039feddad4
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- msm8960.mk
+++ msm8960.mk
@@ -21,15 +21,6 @@
 
 DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
 
-PRODUCT_PACKAGES += \
-	charger_res_images \
-	charger \
-	libwpa_client \
-	hostapd \
-	dhcpcd.conf \
-	wpa_supplicant \
-	wpa_supplicant.conf
-
 # Live Wallpapers
 PRODUCT_PACKAGES += \
 	LiveWallpapers \
@@ -37,18 +28,80 @@ PRODUCT_PACKAGES += \
 	VisualizationWallpapers \
 	librs_jni
 
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
+# Audio
+PRODUCT_PACKAGES += \
+	audio_policy.msm8960 \
+	audio.primary.msm8960 \
+	audio.a2dp.default \
+	audio.usb.default \
+	audio.r_submix.default \
+	libaudio-resampler \
+	audio_effects.conf \
+	audio_policy.conf
 
+# Voice processing
+PRODUCT_PACKAGES += \
+	libqcomvoiceprocessing
+
+# QOS
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/qosmgr_rules.xml:system/etc/qosmgr_rules.xml
 
+# Media
+PRODUCT_PACKAGES += \
+	media_codecs.xml \
+	media_profiles.xml
+
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
-	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+# Graphics
+PRODUCT_PACKAGES += \
+	libgenlock \
+	liboverlay \
+	hwcomposer.msm8960 \
+	gralloc.msm8960 \
+	copybit.msm8960 \
+	memtrack.msm8960
+
+# Codecs
+PRODUCT_PACKAGES += \
+	libmm-omxcore \
+	libdivxdrmdecrypt \
+	libOmxVdec \
+	libOmxVenc \
+	libOmxCore \
+	libstagefrighthw \
+	libc2dcolorconvert
+
+# Camera
+PRODUCT_PACKAGES += \
+	libmmcamera_interface2 \
+	libmmcamera_interface \
+	libxml2	
+
+# WiFi
+PRODUCT_PACKAGES += \
+	libwpa_client \
+	hostapd \
+	dhcpcd.conf \
+	wpa_supplicant \
+	wpa_supplicant.conf \
+	libwfcu \
+	wcnss_service \
+	hostapd_default.conf \
+	wpa_supplicant_overlay.conf \
+	p2p_supplicant_overlay.conf
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+	hci_qcomm_init
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.qualcomm.bt.hci_transport=smd
+
+# Kickstart
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/kickstart_checker.sh:system/etc/kickstart_checker.sh
 
@@ -59,21 +112,33 @@ PRODUCT_PACKAGES += \
 	init.pantech.usb.sh \
 	init.qcom.rc \
 	init.qcom.sh \
+	init.qcom.bt.sh \
+	init.qcom.efs.sync.sh \
+	init.qcom.fm.sh \
+	init.qcom.mdm_links.sh \
+	init.qcom.modem_links.sh \
+	init.qcom.post_boot.sh \
 	init.recovery.qcom.rc \
 	init.target.rc \
-	ueventd.qcom.rc
+	ueventd.qcom.rc \
+
+# Charger
+PRODUCT_PACKAGES += \
+	charger_res_images \
+	charger
+
+# Lights
+PRODUCT_PACKAGES += \
+	lights.qcom
 
 # Prebuilt kl and kcm keymaps
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/hs_detect.kl:system/usr/keylayout/hs_detect.kl \
-	$(LOCAL_PATH)/pmic8xxx_pwrkey.kl:system/usr/keylayout/pmic8xxx_pwrkey.kl \
-	$(LOCAL_PATH)/hs_detect.kcm:system/usr/keychars/hs_detect.kcm \
-	$(LOCAL_PATH)/pmic8xxx_pwrkey.kcm:system/usr/keychars/pmic8xxx_pwrkey.kcm
-
-# Prebuilt input device calibration files
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/touch_dev.idc:system/usr/idc/touch_dev.idc \
-	$(LOCAL_PATH)/touch_dev.kl:system/usr/keylayout/touch_dev.kl
+	$(LOCAL_PATH)/keylayout/hs_detect.kl:system/usr/keylayout/hs_detect.kl \
+	$(LOCAL_PATH)/keylayout/pmic8xxx_pwrkey.kl:system/usr/keylayout/pmic8xxx_pwrkey.kl \
+	$(LOCAL_PATH)/keylayout/hs_detect.kcm:system/usr/keychars/hs_detect.kcm \
+	$(LOCAL_PATH)/keylayout/pmic8xxx_pwrkey.kcm:system/usr/keychars/pmic8xxx_pwrkey.kcm \
+	$(LOCAL_PATH)/keylayout/touch_dev.idc:system/usr/idc/touch_dev.idc \
+	$(LOCAL_PATH)/keylayout/touch_dev.kl:system/usr/keylayout/touch_dev.kl
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
@@ -91,14 +156,32 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
 	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
 	frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
-	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
 # GPS configuration
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/gps.conf:system/etc/gps.conf
+PRODUCT_PACKAGES += \
+	gps.conf
 
+# EGL
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.opengles.version=196608
+
+# Keystore
+PRODUCT_PACKAGES += \
+	keystore.msm8960
+
+# Virtual hardware keys
+PRODUCT_PROPERTY_OVERRIDES += \
+	qemu.hw.mainkeys=0
+
+# QRNGD
+PRODUCT_PACKAGES += qrngd
+
+# RIL
+###############################################################################
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	rild.libpath=/system/lib/libril-qc-qmi-1.so
 
 # Do not power down SIM card when modem is sent to Low Power Mode.
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -107,8 +190,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Ril sends only one RIL_UNSOL_CALL_RING, so set call_ring.multiple to false
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.telephony.call_ring.multiple=0
+###############################################################################
 
-#Upto 3 layers can go through overlays
+# Miscellanous
+###############################################################################
+
+# Upto 3 layers can go through overlays
 PRODUCT_PROPERTY_OVERRIDES += persist.hwc.mdpcomp.enable=true
 
 PRODUCT_TAGS += dalvik.gc.type-precise
@@ -120,39 +207,6 @@ PRODUCT_PACKAGES += \
 # Filesystem management tools
 PRODUCT_PACKAGES += \
 	e2fsck
-
-PRODUCT_PACKAGES += \
-	libgenlock \
-	liboverlay \
-	hwcomposer.msm8960 \
-	gralloc.msm8960 \
-	copybit.msm8960 \
-	memtrack.msm8960
-
-PRODUCT_PACKAGES += \
-	audio_policy.msm8960 \
-	audio.primary.msm8960 \
-	audio.a2dp.default \
-	audio.usb.default \
-	audio.r_submix.default \
-	libaudio-resampler
-
-# Voice processing
-PRODUCT_PACKAGES += libqcomvoiceprocessing
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf
-
-PRODUCT_PACKAGES += \
-	libmm-omxcore \
-	libdivxdrmdecrypt \
-	libOmxVdec \
-	libOmxVenc \
-	libOmxCore \
-	libstagefrighthw \
-	libc2dcolorconvert
-
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	rild.libpath=/system/lib/libril-qc-qmi-1.so
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	drm.service.enabled=true
@@ -175,13 +229,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.qc.sensors.wl_dis=true \
 	ro.qualcomm.sensors.smd=true
 
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	persist.sys.usb.config=mtp
+# For userdebug builds
+ADDITIONAL_DEFAULT_PROPERTIES += \
+	ro.secure=0 \
+	ro.adb.secure=0 \
+	ro.debuggable=1 \
+	persist.sys.root_access=1 \
+	persist.service.adb.enable=1
 
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/fetch-swv:system/bin/fetch-swv
+PRODUCT_PACKAGES += \
+	fetch-swv
 
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/recovery/twrp.fstab:recovery/root/etc/twrp.fstab \
-
+###############################################################################
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
